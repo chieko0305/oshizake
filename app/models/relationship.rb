81a0f8ee18664cb5d363fef11f86e1a50f2dc6c9
EC2 Default User <ec2-user@ip-172-31-13-168.ap-northeast-1.compute.internal>
2023-12-10 06:20:25 +0000

    followの重複修正

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,5 @@
 class Relationship < ApplicationRecord
+  validates_uniqueness_of :follower_id, scope: :followed_id
   # class_name: "User"でUserモデルを参照
   belongs_to :follower, class_name: "User"  #フォローしたユーザー
   belongs_to :followed, class_name: "User"  #フォローされたユーザー
